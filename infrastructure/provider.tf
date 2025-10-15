7361454787f3f2f20e6e070e3e3a97ecef5d12c9
AvihayR <avihayrabi@gmail.com>
2025-10-15 22:25:10 +0300

    Add TF base project

---
+++ infrastructure/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  backend "s3" {
+    bucket = "arize-notes-tf-state-management"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.95.0"
+    }
+  }
+
+}
+
+
+provider "aws" {
+  region = var.region
+}
