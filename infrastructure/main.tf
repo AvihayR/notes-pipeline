5c73ce6588f14351508c68e3c5707253461a18b8
AvihayR <avihayrabi@gmail.com>
2025-10-18 02:06:43 +0300

    Integrate new modules into main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -62,3 +62,38 @@ module "doc_db" {
   subnet_ids          = [for s in module.private_subnets : s.id]
 
 }
+
+module "vpc_endpoints" {
+  source          = "./modules/vpc_endpoints"
+  vpc_id          = module.vpc.vpc_id
+  region          = var.region
+  route_table_ids = [for s in module.private_rt : s.id]
+  subnet_ids      = [for s in module.private_subnets : s.id]
+}
+
+module "ecr" {
+  source     = "./modules/ecr"
+  for_each   = { backend = "notes-backend", frontend = "notes-frontend" }
+  vpc_id     = module.vpc.vpc_id
+  aws_region = var.region
+  repo_name  = each.value
+}
+
+module "alb" {
+  source            = "./modules/alb"
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_ids = [for s in module.public_subnets : s.id]
+}
+
+module "ecs" {
+  source              = "./modules/ecs"
+  aws_region          = var.region
+  vpc_id              = module.vpc.vpc_id
+  ecr_repo_urls       = { backend = module.ecr["backend"].url, frontend = module.ecr["frontend"].url }
+  alb_tg_backend_arn  = module.alb.backend_tg_arn
+  alb_tg_frontend_arn = module.alb.frontend_tg_arn
+  backend_url         = "${module.alb.alb_url}/api"
+  db_url              = module.doc_db.url
+  alb_sg_ids          = [module.alb.sg_id]
+  private_subnet_ids  = [for s in module.private_subnets : s.id]
+}
