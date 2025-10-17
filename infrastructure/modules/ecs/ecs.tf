c0824efaaa6f05706ed48110161dcf0d936fd5bd
AvihayR <avihayrabi@gmail.com>
2025-10-18 02:04:34 +0300

    Refine ECS module

--- infrastructure/modules/ecs/ecs.tf
+++ infrastructure/modules/ecs/ecs.tf
@@ -2,40 +2,43 @@ resource "aws_ecs_cluster" "notes-cluster" {
   name = "notes-containers-cluster"
 }
 
-resource "aws_ecs_task_definition" "notes-app-task" {
-  family = "notes-tasks"
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+
+resource "aws_ecs_task_definition" "backend" {
+  family                   = "notes-backend"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 512
+  memory                   = 1024
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
   container_definitions = jsonencode([
     {
-      "name"      = "notes-frontend",
-      "image"     = "${var.repo_url}/notes-frontend:latest",
-      "cpu"       = 256,
-      "memory"    = 512,
-      "essential" = true,
-      "portMappings" = [
-        {
-          "containerPort" = 80,
-          "hostPort"      = 80,
-          "protocol"      = "tcp"
-        }
-      ],
-      "environment" = [
-        {
-          "name"  = "API_URL"
-          "value" = var.backend_url
-        }
-      ],
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          "awslogs-group"         = aws_cloudwatch_log_group.frontend.name
-          "awslogs-region"        = var.aws_region
-          "awslogs-stream-prefix" = "frontend"
-        }
-      }
-    },
-    {
       "name"      = "notes-backend",
-      "image"     = "${var.repo_url}/notes-backend:latest",
+      "image"     = "${var.ecr_repo_urls.backend}:latest",
       "cpu"       = 256,
       "memory"    = 512,
       "essential" = true,
@@ -64,12 +67,47 @@ resource "aws_ecs_task_definition" "notes-app-task" {
   ])
 }
 
-resource "aws_ecs_service" "notes-service" {
-  name            = "notes-app-service"
-  cluster         = aws_ecs_cluster.notes-cluster.id
-  task_definition = aws_ecs_task_definition.notes-app-task.arn
-  desired_count   = 2
+resource "aws_ecs_task_definition" "frontend" {
+  family                   = "notes-frontend"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 512
+  memory                   = 1024
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      "name"      = "notes-frontend",
+      "image"     = "${var.ecr_repo_urls.frontend}:latest",
+      "cpu"       = 256,
+      "memory"    = 512,
+      "essential" = true,
+      "portMappings" = [
+        {
+          "containerPort" = 80,
+          "hostPort"      = 80,
+          "protocol"      = "tcp"
+        }
+      ],
+      "environment" = [
+        {
+          "name"  = "API_URL"
+          "value" = var.backend_url
+        }
+      ],
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.frontend.name
+          "awslogs-region"        = var.aws_region
+          "awslogs-stream-prefix" = "frontend"
+        }
+      }
+    }
+  ])
 }
+
+
 
 resource "aws_cloudwatch_log_group" "backend" {
   name              = "/ecs/backend"
@@ -89,6 +127,104 @@ resource "aws_cloudwatch_log_group" "frontend" {
   }
 }
 
-output "name" {
-  value = aws_ecs_cluster.notes-cluster.name
+resource "aws_security_group" "frontend" {
+  name        = "frontend-sg"
+  description = "Allow ALB to reach frontend containers"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description     = "Allow HTTP traffic from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = var.alb_sg_ids
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "frontend-sg"
+  }
+}
+
+
+resource "aws_security_group" "backend" {
+  name        = "backend-sg"
+  description = "Allow ALB to reach frontend containers"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description     = "Allow HTTP traffic from ALB"
+    from_port       = 8000
+    to_port         = 8000
+    protocol        = "tcp"
+    security_groups = var.alb_sg_ids
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "frontend-sg"
+  }
+}
+
+
+resource "aws_ecs_service" "notes-frontend" {
+  name            = "notes-frontend"
+  cluster         = aws_ecs_cluster.notes-cluster.id
+  task_definition = aws_ecs_task_definition.frontend.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.private_subnet_ids
+    security_groups  = [aws_security_group.frontend.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = var.alb_tg_frontend_arn
+    container_name   = "notes-frontend"
+    container_port   = 80
+  }
+
+  tags = {
+    Name = "notes-frontend-service"
+  }
+}
+
+resource "aws_ecs_service" "backend" {
+  name            = "notes-backend"
+  cluster         = aws_ecs_cluster.notes-cluster.id
+  task_definition = aws_ecs_task_definition.backend.arn
+  desired_count   = 2
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = var.private_subnet_ids
+    security_groups  = [aws_security_group.backend.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = var.alb_tg_backend_arn
+    container_name   = "notes-backend"
+    container_port   = 8000
+  }
+
+  tags = {
+    Name = "notes-backend-service"
+  }
 }
--- infrastructure/modules/ecs/variables.tf
+++ infrastructure/modules/ecs/variables.tf
@@ -1,5 +1,9 @@
-variable "repo_url" {
+variable "vpc_id" {
   type = string
+}
+
+variable "ecr_repo_urls" {
+  type = map(string)
 }
 
 variable "backend_url" {
@@ -14,3 +18,18 @@ variable "db_url" {
   type = string
 }
 
+variable "private_subnet_ids" {
+  type = list(string)
+}
+
+variable "alb_tg_frontend_arn" {
+  type = string
+}
+
+variable "alb_tg_backend_arn" {
+  type = string
+}
+
+variable "alb_sg_ids" {
+  type = list(string)
+}
